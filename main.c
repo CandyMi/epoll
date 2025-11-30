0cc432533bae23d38382490ceb69ccba32e0a286
869646063@qq.com <869646063@qq.com>
2025-11-30 14:15:22 +0800

    test: 增加更多的测试用例

--- main.c
+++ main.c
@@ -36,6 +36,91 @@ int64_t time_now()
     return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
+EPOLL_TEST_FUNCTION(testcase_epoll_oneshot, {
+#ifndef WIN32
+    HANDLE efd = epoll_create(0);
+    assert(efd > 0); int r;
+    int fds[2]; pipe(fds);
+    int RPIPE = fds[0];
+    int WPIPE = fds[1];
+    struct epoll_event event[1];
+    struct epoll_event ev;
+    ev.events = EPOLLIN | EPOLLONESHOT;
+    ev.data.fd = RPIPE;
+    r = epoll_ctl(efd, EPOLL_CTL_ADD, RPIPE, &ev);
+    assert(r == 0);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 0);
+    write(WPIPE, "1", 1);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 1);
+    assert(event[0].data.fd == RPIPE);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 0);
+    epoll_close(efd);
+#endif
+})
+
+EPOLL_TEST_FUNCTION(testcase_epoll_et_mode, {
+#ifndef WIN32
+    HANDLE efd = epoll_create(0);
+    assert(efd > 0); int r;
+    int fds[2]; pipe(fds);
+    int RPIPE = fds[0];
+    int WPIPE = fds[1];
+    struct epoll_event event[1];
+    struct epoll_event ev;
+    ev.events = EPOLLIN | EPOLLET;
+    ev.data.fd = RPIPE;
+    r = epoll_ctl(efd, EPOLL_CTL_ADD, RPIPE, &ev);
+    assert(r == 0);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 0);
+    write(WPIPE, "1", 1);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 1);
+    assert(event[0].data.fd == RPIPE);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 0);
+    r = epoll_ctl(efd, EPOLL_CTL_MOD, RPIPE, &ev);
+    assert(r == 0);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 1);
+    r = epoll_wait(efd, event, 1, 10);
+    assert(r == 0);
+    epoll_close(efd);
+#endif
+})
+
+EPOLL_TEST_FUNCTION(testcase_epoll_repeat, {
+#ifndef WIN32
+    HANDLE efd = epoll_create(0);
+    assert(efd > 0); int r;
+    int fds[2]; pipe(fds);
+    int RPIPE = fds[0];
+    int WPIPE = fds[1];
+    struct epoll_event event[1];
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = RPIPE;
+    r = epoll_ctl(efd, EPOLL_CTL_ADD, RPIPE, &ev);
+    assert(r == 0);
+    write(WPIPE, "1", 1);
+    for (size_t i = 0; i < 100000; i++)
+    {
+        r = epoll_wait(efd, event, 1, 0);
+        assert(event[0].data.fd == RPIPE);
+        assert(r == 1);
+    }
+    char buf [128];
+    r = read(RPIPE, buf, 128);
+    assert(r >= 1);
+    r = epoll_wait(efd, event, 1, 0);
+    assert(r == 0);
+    epoll_close(efd);
+#endif
+})
+
 EPOLL_TEST_FUNCTION(testcase_epoll_watch, {
 #ifndef WIN32
     char sendbuf[] = "123";
@@ -51,9 +136,9 @@ EPOLL_TEST_FUNCTION(testcase_epoll_watch, {
     assert(r == 0);
     struct epoll_event event[1];
     r = write(WPIPE, sendbuf, strlen(sendbuf));
-    assert(r = strlen(sendbuf));
+    assert(r == strlen(sendbuf));
     r = epoll_wait(efd, event, 1, 10);
-    assert(r = 1);
+    assert(r == 1);
     assert(event[0].data.fd == RPIPE);
     assert(event[0].events == EPOLLIN);
     char *buf = malloc(strlen(sendbuf));
@@ -61,7 +146,7 @@ EPOLL_TEST_FUNCTION(testcase_epoll_watch, {
     assert(r == strlen(sendbuf));
     free(buf); epoll_close(efd);
 #endif
-});
+})
 
 EPOLL_TEST_FUNCTION(testcase_epoll_alloc, 
 {
@@ -95,5 +180,8 @@ int main(int argc, char const *argv[])
     testcase_epoll_timer();
     testcase_epoll_alloc();
     testcase_epoll_watch();
+    testcase_epoll_repeat();
+    testcase_epoll_oneshot();
+    testcase_epoll_et_mode();
     return 0;
 }
